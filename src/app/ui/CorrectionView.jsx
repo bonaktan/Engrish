355e69bdb804fd60120111c8f6925b4bc036b9b5
bonaktan-onotherpcs <02.bonnybonnybonaktan@gmail.com>
2025-01-31 11:33:52 +0800

    CorrectionView Designing

--- src/app/ui/CorrectionView.jsx
+++ src/app/ui/CorrectionView.jsx
@@ -1,8 +1,14 @@
 export default function CorrectionView({ correction }) {
     // correction is a string that contains the corrections na sinuggest ni grmmar checker
     return (
-        <>
+       <div class="darkBlue">
+        <div class="blue">
             <p>{correction} - correction</p>
-        </>
+            
+        </div>
+     </div> 
+ 
     );
 }
+
+
