5278aef828dc9f8769c070d9bc59b06d4d8dd62e
bonaktan <128207489+bonaktan@users.noreply.github.com>
2025-02-01 19:34:19 +0800

    Revert "Initial Movement from NodeJS-Backend -> Python"
    This reverts commit 9a04d96da61e404e8958e3f662bb2d887af2094c.

--- components/backend.py
+++ components/backend.py
@@ -1,19 +1,10 @@
-import asyncio
-from websockets.asyncio.server import serve
-
-async def echo(websocket):
-    async for message in websocket:
-        print(message)
-        await websocket.send(message + "test")
-
-async def main():
-    async with serve(echo, "localhost", 8765):
-        await asyncio.get_running_loop().create_future()  # run forever
-
-asyncio.run(main())
-# @route("/api/userInput")
-# def microphoneInput():
-#     return "isang malaking tite"
+from bottle import app, route, run
 
 
-# run(host="127.0.0.1", port=8000, reloader=True)
+
+@route("/api/userInput")
+def microphoneInput():
+    return "isang malaking tite"
+
+
+run(host="127.0.0.1", port=8000, reloader=True)
--- src/app/frontend/appContext.js
+++ src/app/frontend/appContext.js
@@ -1,46 +1,25 @@
 "use client";
-import { useState, useReducer, useEffect } from "react";
+import { useState, useReducer } from "react";
+import speechToText from "../backend/speechtotext";
 import { Message } from "../backend/structures";
-import { io } from "socket.io-client";
-
 
 export default function useConversation() {
     const [convo, addConvo] = useReducer(updateConvo, [new Message("ai", "testing Engrish if the components are properly linked to each other - useConversation()")]);
     const [correction, setCorrection] = useState("-");
-    const [latestMessage, setLatestMessage] = useState("")
     async function handleAddConvo(e) {
         // entrypoint of the convo loop, initiated in app/ui/UserControls.jsx
         e.preventDefault();
-
-        setLatestMessage("wala pa kong audio functionality here, kunwari nagsalita ako ng ganto - useConversation()"); // TODO: audio component
-        addConvo({ sender: "user", message: latestMessage }); // calls updateConvo()
-        var { grammarFeedback, llmGeneration } = await correctionLoop(latestMessage);
-
-        setCorrection(grammarFeedback);
-        addConvo({ sender: "ai", message: llmGeneration });
+        const userInput = "wala pa kong audio functionality here, kunwari nagsalita ako ng ganto - useConversation()"; // TODO: audio component
+        addConvo({sender: "user", message: userInput}); // calls updateConvo()
+        var { grammarFeedback, llmGeneration } = await speechToText(userInput); // TODO: Refactor
+        setCorrection(grammarFeedback.output);
+        addConvo({sender: "ai", message: llmGeneration.output});
         // TODO: play the audio output that llmGeneration.audio gives
     }
-    useEffect(() => {
-        // Create a socket connection
-        const socket = io("ws://localhost:8765");
-
-        // Listen for incoming messages
-        socket.on('message', (message) => {
-            addConvo({sender: "ai", message: message})
-        });
-
-        // Clean up the socket connection on unmount
-        return () => socket.disconnect();
-    }, []);
     return { convo, correction, handleAddConvo, setCorrection };
 }
 
 function updateConvo(convo, message) {
 
     return [...convo, new Message(message.sender, message.message)]; // WARN: no data processing is done
-}
-
-async function correctionLoop(userInput) {
-    // use socket.io please
-    return {grammarFeedback: "test", llmGeneration: "test"}
 }
