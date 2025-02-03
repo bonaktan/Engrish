0dbd8f6369f163ed4f7a6c16dada33f323e637d6
bonaktan <128207489+bonaktan@users.noreply.github.com>
2025-02-03 21:51:35 +0800

    Initial Setup on Sidebar Feature

--- src/app/ui/Header.jsx
+++ src/app/ui/Header.jsx
@@ -2,7 +2,9 @@ export default function Header() {
     const version = "0.2.0"
     return (
         <div className="text-center header flex justify-evenly items-center flex-grow-0 flex-shrink-0">
-            <div>{/* TODO: menu */}</div>
+            <div>
+                <button>Sidebar</button>
+            </div>
             <div>
                 <h1 className="text-xl">Engrish - Development</h1>
                 <p>Ver. {version}</p>
