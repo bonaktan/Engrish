e37885320b205cc24784e3f94d9dd88267d8eefe
bonaktan <128207489+bonaktan@users.noreply.github.com>
2025-02-03 21:41:42 +0800

    Flip-flop Logic added on Button

--- src/app/frontend/useControls.js
+++ src/app/frontend/useControls.js
@@ -1,9 +1,20 @@
-"use client"
+"use client";
 
-import { useReducer } from "react"
+import { useEffect, useReducer, useState } from "react";
 
 export default function useUserControls() {
     // RESPONSIBILITY: control UserControls
-    const [recording, toggleRecording] = useReducer((state) => {return !state}, false)
-    return {recording, toggleRecording}
+    const [recording, setRecording] = useState(false);
+    var audio = null
+    function startRecording() {
+        console.log("start")
+        // START: recording of voices
+        setRecording(true)
+    }
+    function stopRecording() {
+        console.log("stop")
+        // STOP: recording of voices
+        setRecording(false)
+    }
+    return { recording, startRecording, stopRecording};
 }
