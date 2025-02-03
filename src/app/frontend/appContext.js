6372c257109c6e59e87f77b79ed84212f6af5154
bonaktan-onotherpcs <02.bonnybonnybonaktan@gmail.com>
2025-02-03 16:02:41 +0800

    Initial feature Implementation

--- src/app/frontend/appContext.js
+++ src/app/frontend/appContext.js
@@ -9,11 +9,45 @@ export default function useConversation() {
     async function handleAddConvo(e) {
         // entrypoint of the convo loop, initiated in app/ui/UserControls.jsx
         e.preventDefault();
-        const userInput = "wala pa kong audio functionality here, kunwari nagsalita ako ng ganto - useConversation()"; // TODO: audio component
-        addConvo({sender: "user", message: userInput}); // calls updateConvo()
-        var { grammarFeedback, llmGeneration } = await speechToText(userInput); // TODO: Refactor
-        setCorrection(grammarFeedback.output);
-        addConvo({sender: "ai", message: llmGeneration.output});
+        let stream;
+        let recorder;
+
+        stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+        recorder = new MediaRecorder(stream);
+
+
+        stopMicrophoneButton.addEventListener("click", () => {
+            // Stop the stream.
+            stream.getTracks().forEach(track => track.stop());
+        });
+
+        startRecordButton.addEventListener("click", async () => {
+            // For the sake of more legible code, this sample only uses the
+            // `showSaveFilePicker()` method. In production, you need to
+            // cater for browsers that don't support this method, as
+            // outlined in https://web.dev/patterns/files/save-a-file/.
+
+            // Prompt the user to choose where to save the recording file.
+            const suggestedName = "microphone-recording.webm";
+            const handle = await window.showSaveFilePicker({ suggestedName });
+            const writable = await handle.createWritable();
+
+            // Start recording.
+            recorder.start();
+            recorder.addEventListener("dataavailable", async (event) => {
+                // Write chunks to the file.
+                await writable.write(event.data);
+                if (recorder.state === "inactive") {
+                    // Close the file when the recording stops.
+                    await writable.close();
+                }
+            });
+        });
+
+        stopRecordButton.addEventListener("click", () => {
+            // Stop the recording.
+            recorder.stop();
+        });
         // TODO: play the audio output that llmGeneration.audio gives
     }
     return { convo, correction, handleAddConvo, setCorrection };
