32a9fb64cf2231b47d7c309f75f1c73fd1fcda21
bonaktan <128207489+bonaktan@users.noreply.github.com>
2025-02-03 21:41:42 +0800

    Flip-flop Logic added on Button

--- src/app/ui/UserControls.jsx
+++ src/app/ui/UserControls.jsx
@@ -2,9 +2,10 @@ import useUserControls from "../frontend/useControls";
 export default function UserControls() {
     // handleMicrophone handles everything here
     // di nyo yun sakop pero thats defined in app/frontend/controls.js
+    const {recording, toggleRecording, startRecording, stopRecording} = useUserControls()
     return (
         <div className="user-input flex-grow-0 flex-shrink-0 flex justify-evenly">
-            <button className="input-button">Simulate</button>
+            <button className="input-button" onClick={recording ? stopRecording : startRecording}>{recording ? "Recording" : "Record"}</button>
         </div>
     );
 }
