12cec25bd168a12f0bae47c1c9f98548c6d0f4c5
bonaktan <128207489+bonaktan@users.noreply.github.com>
2025-02-06 20:22:59 +0800

    Designing of Sidebar implemented

--- src/app/ui/SidebarView.jsx
+++ src/app/ui/SidebarView.jsx
@@ -1,14 +1,26 @@
-export default function SidebarView({open}) {
-  // dito ka magtrabaho sa design and technicals, if need ng tulong sa js call me
-  return (
-    <div className={"sideBar " + (open ? "block" : "hidden")}>
-      <div className="sidebarAccess">
-        <ul>
-          <li>
-            <a href="#about">About Us</a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  );
+export default function SidebarView({ open }) {
+    // dito ka magtrabaho sa design and technicals, if need ng tulong sa js call me
+    return (
+        <div className={"sideBar " + (open ? "flex" : "hidden")}>
+            <div className="sidebarHeader">
+                <p>ENGRISH</p>
+                <button>X</button>
+            </div>
+            <div className="sidebarOptions">
+                <div className="option" id="optionTTSEngine">
+                    <p>Text-to-Speech Engine</p>
+                    <div className="selection">
+                        <label>pyttsx3 <input type="radio"/></label>
+                        <label>gtts <input type="radio"/></label>
+                    </div>
+                </div>
+                <div className="option" id="optionAIVoice">
+                    <p><label>AI Voice <input type="checkbox"/></label></p>
+                </div>
+            </div>
+            <div className="aboutUs">
+                <div>About Us</div>
+            </div>
+        </div>
+    );
 }
