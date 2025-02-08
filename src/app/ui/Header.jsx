a14dc557c7b7c3f779b4ed8d5d859da4ac575358
bonaktan <128207489+bonaktan@users.noreply.github.com>
2025-02-08 21:42:13 +0800

    Version 1.0.0 - Fully working Proof-of-Concept

--- src/app/ui/Header.jsx
+++ src/app/ui/Header.jsx
@@ -1,10 +1,10 @@
 export default function Header({toggleSidebar}) {
-  const version = "0.4.0";
+  const version = "1.0.0";
   return (
     <div className="text-center header flex justify-evenly items-center flex-grow-0 flex-shrink-0">
       <button className="menu" onClick={toggleSidebar}>Sidebar</button>
       <div>
-        <h1 className="text-xl">Engrish - Development</h1>
+        <h1 className="text-xl">Engrish</h1>
         <p>Ver. {version}</p>
       </div>
       <div>{/* TODO: history */}</div>
