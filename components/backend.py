69a24631a813c8682b7c2259bfd360e5a2595ac0
bonaktan <128207489+bonaktan@users.noreply.github.com>
2025-02-09 17:14:07 +0800

    Initial HTTPS Migration

--- components/backend.py
+++ components/backend.py
@@ -1,17 +1,17 @@
 from aiohttp import web
-import asyncio
 import socketio
-from time import sleep
-import gtts, os, io
-import pyttsx3
 import base64
-import speech_recognition as sr
 import ffmpeg
-from dotenv import load_dotenv
 import components
-
+import ssl
+import aiohttp_cors
 AVOID_TOKEN_USAGES = True
-sio = socketio.AsyncServer(cors_allowed_origins="*")
+AVOID_MEMORY_USAGE = True
+sio = socketio.AsyncServer(async_mode='aiohttp', ccors_allowed_origins=['*'])
+ssl_context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+ssl_context.load_cert_chain(
+    "../certificates/ResearchGroup2.crt", "../certificates/ResearchGroup2Private.key"
+)
 webAPI = web.Application()
 sio.attach(webAPI)
 
@@ -29,7 +29,8 @@ def connect(sid, environ):
 @sio.event
 async def user_input(sid, userInput):
     print("message ", userInput)
-    if userInput == None: return
+    if userInput == None:
+        return
     # decoding b64-encoded wav file
     with open("preprocessed.wav", "wb") as fh:
         fh.write(base64.decodebytes(userInput.encode("ascii")))
@@ -49,17 +50,35 @@ async def user_input(sid, userInput):
     ttsEngine.speak(llmOut)
     print("Message Transaction done")
 
+
 @sio.event
 def disconnect(sid):
     print("disconnect ", sid)
 
 
 if __name__ == "__main__":
-    sttEngine = components.SpeechToText()
+    if AVOID_MEMORY_USAGE:
+        print("WARNING: STT and Grammar Checker are disabled.")
+    if AVOID_TOKEN_USAGES:
+        print("WARNING: LLM is disabled.")
+    sttEngine = components.SpeechToText(simulate=AVOID_MEMORY_USAGE)
     llmModel = components.LargeLanguageModel(simulate=AVOID_TOKEN_USAGES)
-    checkerModel = components.GrammarChecker()
+    checkerModel = components.GrammarChecker(simulate=AVOID_MEMORY_USAGE)
     ttsEngine = components.TextToSpeech(engineUsed="gtts")
     webAPI.router.add_get("/", index)
-    web.run_app(webAPI, port=8765)
+    cors = aiohttp_cors.setup(
+        webAPI,
+        defaults={
+            "*": aiohttp_cors.ResourceOptions(
+                allow_credentials=True, expose_headers="*", allow_headers="*"
+            )
+        },
+    )
+
+    for route in list(webAPI.router.routes()): 
+        # print(dir(route.resource.raw_match()))
+        if route.resource.raw_match("/socket.io/"): continue
+        cors.add(route)
+    web.run_app(webAPI, port=8765, ssl_context=ssl_context)
 
 # test sentence: I do be testing ENGRISH right now, does this working now?
